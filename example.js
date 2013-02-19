8618d083e7b3ed2cab9789f3e46274d0e96d3648
Gene Hallman <gene@flickme.com>
2013-02-18 22:02:48 -0800

    added examples for labs-portal

---
+++ example.js
@@ -0,0 +1,25 @@
+define(function(require) {
+	var Hub = require('streamhub-backbone');
+	var View = require('streamhub-timeline');
+
+	return function(sdk, opts) {
+		document.getElementById(opts.elementId).innerHTML += "<div class='score1'>0</div>"+
+    		"<div class='score2'>0</div>"+
+    		"<div class='quarter'></div>";
+		
+        var col = window.col = new Hub.Collection().setRemote({
+            sdk: sdk,
+            siteId: opts.siteId,
+            articleId: opts.articleId3
+        });
+
+		var view = new View({
+			collection: col,
+			el: document.getElementById(opts.elementId),
+		});
+
+        view.render();
+        
+        return view;
+    };
+});
