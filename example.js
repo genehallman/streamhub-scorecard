8b5739294391732b4dbdb4f9057143e1f0178d8c
Gene Hallman <genehallman@gmail.com>
2013-05-09 13:55:27 -0600

    Update example.js

--- example.js
+++ example.js
@@ -2,17 +2,13 @@ define(function(require) {
 	var Hub = require('streamhub-sdk');
 	var View = require('streamhub-scorecard');
 
-	return function(sdk, opts) {
+	return function(el) {
 		document.getElementById(opts.elementId).innerHTML += [
 			"Score 1: <div class='score1'>0</div>",
-    		"Score 2: <div class='score2'>0</div>",
-    		"Quarter: <div class='quarter'></div>"].join('');
+    			"Score 2: <div class='score2'>0</div>",
+    			"Quarter: <div class='quarter'></div>"].join('');
 		
-		var view = new View({
-			streams: Hub.Streams.forCollection($.extend({}, opts, {articleId: opts.articleId1})).start()
-			el: document.getElementById(opts.elementId),
-		});
-        
-        return view;
-    };
+		var view = new View({el: el});
+        	return view;
+	};
 });
