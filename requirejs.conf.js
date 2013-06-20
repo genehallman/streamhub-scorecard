26e3792d3e2461ad1d2f3579a363187198ad2360
Benjamin Goering <bengoering@gmail.com>
2013-06-19 18:01:45 -0700

    Add streamhub-zepto to requirejs.conf.js

--- requirejs.conf.js
+++ requirejs.conf.js
@@ -1,13 +1,14 @@
 require.config({
   paths: {
     jquery: 'lib/jquery/jquery',
-	jasmine: 'lib/jasmine/lib/jasmine-core/jasmine',
+	  jasmine: 'lib/jasmine/lib/jasmine-core/jasmine',
     'jasmine-html': 'lib/jasmine/lib/jasmine-core/jasmine-html',
     'jasmine-jquery': 'lib/jasmine-jquery/lib/jasmine-jquery',
     text: 'lib/requirejs-text/text',
     hgn: 'lib/requirejs-hogan-plugin/hgn',
     hogan: 'lib/hogan/web/builds/2.0.0/hogan-2.0.0.amd',
-    base64: 'lib/base64/base64'
+    base64: 'lib/base64/base64',
+    'streamhub-zepto': 'lib/streamhub-zepto/build/zepto.min'
   },
   packages: [{
     name: 'streamhub-sdk',
@@ -35,6 +36,5 @@ require.config({
     jquery: {
         exports: '$'
     }
-  },
-  urlArgs: "_=" +  (new Date()).getTime()
+  }
 });
